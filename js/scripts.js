14a736563f624f57fb050a5d878f341db79897e2
mhyneman8 <mallory.hyneman@gmail.com>
2021-10-16 18:46:29 -0500

    update index page and dropdown menu

---
+++ js/scripts.js
@@ -0,0 +1,66 @@
+// set up text to print, each item in array is new line
+var aText = new Array(
+    "Mallory Hyneman", 
+    "Web Development"
+    );
+    var iSpeed = 150; // time delay of print out
+    var iIndex = 0; // start printing array at this posision
+    var iArrLength = aText[0].length; // the length of the text array
+    var iScrollAt = 20; // start scrolling up at this many lines
+     
+    var iTextPos = 0; // initialise text position
+    var sContents = ''; // initialise contents variable
+    var iRow; // initialise current row
+     
+    function typewriter()
+    {
+     sContents =  ' ';
+     iRow = Math.max(0, iIndex-iScrollAt);
+     var destination = document.getElementById("typedtext");
+     
+     while ( iRow < iIndex ) {
+      sContents += aText[iRow++] + '<br />';
+     }
+     destination.innerHTML = sContents + aText[iIndex].substring(0, iTextPos) + "_";
+     
+     if ( iTextPos++ == iArrLength ) {
+      iTextPos = 0;
+      iIndex++;
+      if ( iIndex != aText.length ) {
+       iArrLength = aText[iIndex].length;
+       setTimeout("typewriter()", 500);
+      }
+     } else {
+      setTimeout("typewriter()", iSpeed);
+     }
+    }
+    
+    setTimeout("typewriter()", 5100);
+    // typewriter()
+
+function dropdown() {
+    const dropbtn = document.querySelector('.dropbtn');
+    dropbtn.addEventListener('click', show);
+
+    function show() {
+        const content = document.querySelector('.dropdown-content');
+
+        content.classList.add('show');
+    }
+}
+dropdown();
+
+function close() {
+    const closeBtn = document.querySelector('.close');
+    closeBtn.addEventListener('click', close);
+ 
+    function close() {
+        const content = document.querySelector('.dropdown-content');
+        if (content.classList.contains('show')) {
+            content.classList.remove('show');
+        }
+        // content.classList.remove('show');
+        // content.addClass('hide');
+    }
+}
+close();
